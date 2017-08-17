1e6c8bcaa18043f9ac34c2a0dbe91cb0c332471c
Samuel Gómez <samuel.gomez@unidadeditorial.es>
2017-08-17 19:51:37 +0200

    Connect Redux to Individual React Components For More Efficient Rendering

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import {updateCurrent} from '../reducers/todo'
 
-export default (props) => {
-  const {currentTodo, changeCurrent} = props
+
+const TodoForm = (props) => {
+  const {currentTodo, updateCurrent} = props
   const handleInputChange = (evt) => {
     const val = evt.target.value
-    changeCurrent(val)
+    updateCurrent(val)
   }
   return <form>
     <input type="text"
@@ -12,3 +15,4 @@ export default (props) => {
       value={currentTodo}/>
   </form>
 }
+export default connect((state)=>({currentTodo: state.currentTodo}), {updateCurrent})(TodoForm)
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import TodoItem from './TodoItem'
+import { connect } from 'react-redux'
 
-export default (props) =>  (
+const TodoList = (props) =>  (
   <div className="Todo-List">
     <ul>
       {props.todos.map( todo => <TodoItem key={todo.id} {...todo} />)}
     </ul>
   </div>
 )
+export default connect((state)=>({todos: state.todos}))(TodoList)
