f77b6992635418a8e58cde140fd93c82cae3b519
Samuel Gómez <samuel.gomez@unidadeditorial.es>
2017-08-19 17:11:25 +0200

    Add json DB and fetch todos with thunk and fetch from database server

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,12 +1,21 @@
-import React from 'react'
+import React, {Component} from 'react'
 import TodoItem from './TodoItem'
 import { connect } from 'react-redux'
+import {fetchTodos} from '../reducers/todo'
 
-const TodoList = (props) =>  (
-  <div className="Todo-List">
-    <ul>
-      {props.todos.map( todo => <TodoItem key={todo.id} {...todo} />)}
-    </ul>
-  </div>
-)
-export default connect((state)=>({todos: state.todos}))(TodoList)
+
+class TodoList extends Component {
+  componentDidMount(){
+    this.props.fetchTodos();
+  }
+  render(){
+    return (
+      <div className="Todo-List">
+        <ul>
+          {this.props.todos.map( todo => <TodoItem key={todo.id} {...todo} />)}
+        </ul>
+      </div>
+    )
+  }
+}
+export default connect((state)=>({todos: state.todos}), {fetchTodos})(TodoList)
--- src/reducers/todo.js
+++ src/reducers/todo.js
@@ -1,19 +1,19 @@
+import {getTodos} from '../lib/todoServices'
 const initialState = {
-  todos: [
-    {id: 1, name: 'Render static UI', isComplete: true},
-    {id: 2, name: 'Create initial state', isComplete: true},
-    {id: 3, name: 'Render based on state', isComplete: false},
-  ],
+  todos: [],
   currentTodo: ''
 }
 //ACTIONS TYPES
 const TODO_ADD = 'TODO_ADD'
+const TODOS_LOAD = 'TODOS_LOAD'
 const CURRENT_UPDATE = 'CURRENT_UPDATE'
 //ACTIONS
 export default (state = initialState, action) => {
   switch (action.type) {
     case TODO_ADD:
       return {...state, todos:[...state.todos, action.payload]}
+    case TODOS_LOAD:
+      return {...state, todos: action.payload}
     case CURRENT_UPDATE:
       return {...state, currentTodo: action.payload}
     default:
@@ -26,3 +26,11 @@ export const updateCurrent = (val) => (
   {type: 'CURRENT_UPDATE',
   payload: val}
 );
+
+export const loadTodos = (todos) => ({type:TODOS_LOAD, payload: todos})
+
+export const fetchTodos = () => {
+  return (dispatch) => {
+    getTodos().then(todos => dispatch(loadTodos(todos)))
+  }
+}
